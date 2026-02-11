e2743789ad0b04d6fe247651f648ce831f844fed
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-11 04:45:51 +0400

    Create AuthWelcomeText Component
      Created this component display welcome message in different authentication screen components.

---
+++ src/components/ui/auth-welcome-text/auth-welcome-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-welcome-text',
+  imports: [],
+  templateUrl: './auth-welcome-text.html',
+  styleUrl: './auth-welcome-text.css',
+})
+export class AuthWelcomeText {
+
+}

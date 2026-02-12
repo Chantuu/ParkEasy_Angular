4106b892bd1e98605df06565e714b1ba5e7a3570
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-12 16:17:31 +0400

    Add LoginPage Component Route

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from '../layout/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+];

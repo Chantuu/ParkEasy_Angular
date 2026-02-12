22922253c4aec57928a415514d91730bf4badc01
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-12 17:06:58 +0400

    Add RegisterPage Route

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from '../layout/login-page/login-page';
+import { RegisterPage } from '../layout/register-page/register-page';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginPage,
+  },
+  {
+    path: 'register',
+    component: RegisterPage,
   },
 ];

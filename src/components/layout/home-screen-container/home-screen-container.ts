392094fd553a452b1f41a29750c41129c8eb2ef9
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-12 17:10:52 +0400

    Create HomeScreenContainer Component

---
+++ src/components/layout/home-screen-container/home-screen-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen-container',
+  imports: [],
+  templateUrl: './home-screen-container.html',
+  styleUrl: './home-screen-container.css',
+})
+export class HomeScreenContainer {
+
+}

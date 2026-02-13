ad6fef9531626e182653b7c135a225a999062ad6
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 03:24:36 +0400

    Implement AuthService Functionality
      Implemented authentication management functionality with backend.

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { AuthResponseInterface } from '../utilities/interfaces/responses/auth-response.interface';
+import { LoginRequestBodyInterface } from '../utilities/interfaces/request-bodies/login-request-body.interface';
+import { RegisterRequestBodyInterface } from '../utilities/interfaces/request-bodies/register-request-body.interface';
+import { CurrentUserResponseInterface } from '../utilities/interfaces/responses/current-user-response.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+  constructor(private _apiService: ApiService) {}
+
+  signInUser(requestBody: LoginRequestBodyInterface) {
+    return this._apiService.sendPostRequest<LoginRequestBodyInterface, AuthResponseInterface>(
+      'auth/login',
+      requestBody,
+    );
+  }
+
+  registerUser(requestBody: RegisterRequestBodyInterface) {
+    return this._apiService.sendPostRequest<RegisterRequestBodyInterface, AuthResponseInterface>(
+      'auth/register',
+      requestBody,
+    );
+  }
+
+  checkAuth() {
+    return this._apiService.sendGetRequest<CurrentUserResponseInterface>('auth/currentUser');
+  }
+
+  logOutUser() {}
 }

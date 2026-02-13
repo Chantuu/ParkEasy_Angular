89315e812174cddb9c338c40a53be83312ca4f45
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 03:21:35 +0400

    Implement ApiService
      Implemented this service for defining generic request sending functionality in one place.

---
+++ src/services/api-service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly _apiUrl = import.meta.env.NG_APP_API_URL;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  sendGetRequest<ResponseT>(endpointUrl: string) {
+    return this._httpClient.get<ResponseT>(`${this._apiUrl}/${endpointUrl}`, {
+      withCredentials: true,
+    });
+  }
+
+  sendPostRequest<BodyT, ResponseT>(endpointUrl: string, body: BodyT) {
+    return this._httpClient.post<ResponseT>(`${this._apiUrl}/${endpointUrl}`, body, {
+      withCredentials: true,
+    });
+  }
+}

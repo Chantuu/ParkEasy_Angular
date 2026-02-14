c0a409eadb1b852f4978649c058c1329a8f89a2e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 05:24:28 +0400

    Implement TopBar Component Functionality

--- src/components/ui/top-bar/top-bar.ts
+++ src/components/ui/top-bar/top-bar.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LogoutButton } from '../logout-button/logout-button';
+import { AuthService } from '../../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  imports: [LogoutButton],
   templateUrl: './top-bar.html',
   styleUrl: './top-bar.css',
+  host: {
+    class: 'globalContainerXMargin',
+  },
 })
 export class TopBar {
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
 
+  onLogoutPress() {
+    this._authService.logOutUser().subscribe({
+      next: () => {
+        this._router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }

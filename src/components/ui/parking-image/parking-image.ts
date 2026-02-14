86211d2a4ee7019d133938c389cae1af778cfc10
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 20:59:17 +0400

    Create ParkingImage Component

---
+++ src/components/ui/parking-image/parking-image.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-image',
+  imports: [],
+  templateUrl: './parking-image.html',
+  styleUrl: './parking-image.css',
+})
+export class ParkingImage {
+
+}

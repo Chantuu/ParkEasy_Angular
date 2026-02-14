499acb3e537baefcfb2f36e2237770825a421ae8
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 22:10:18 +0400

    Create ParkingSpotCar Component

---
+++ src/components/ui/parking-spot-card/parking-spot-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-spot-card',
+  imports: [],
+  templateUrl: './parking-spot-card.html',
+  styleUrl: './parking-spot-card.css',
+})
+export class ParkingSpotCard {
+
+}

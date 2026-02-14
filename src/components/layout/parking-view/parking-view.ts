9414be822fd961d940d1b0907dc1324da6d4f3ce
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 20:45:38 +0400

    Create ParkingView Component

---
+++ src/components/layout/parking-view/parking-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-view',
+  imports: [],
+  templateUrl: './parking-view.html',
+  styleUrl: './parking-view.css',
+})
+export class ParkingView {
+
+}

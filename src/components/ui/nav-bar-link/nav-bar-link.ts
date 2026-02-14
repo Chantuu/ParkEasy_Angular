40ad1f211bd2cfad22cac69e46f4eb02dc6db233
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 06:32:10 +0400

    Create NavBarLink Component

---
+++ src/components/ui/nav-bar-link/nav-bar-link.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-link',
+  imports: [],
+  templateUrl: './nav-bar-link.html',
+  styleUrl: './nav-bar-link.css',
+})
+export class NavBarLink {
+
+}

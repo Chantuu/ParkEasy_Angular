e8ab244f5635c9f9e815bf89d05378a1549770a7
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 22:09:59 +0400

    Create ParkingSpotList Component

---
+++ src/components/layout/parking-spot-list/parking-spot-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-spot-list',
+  imports: [],
+  templateUrl: './parking-spot-list.html',
+  styleUrl: './parking-spot-list.css',
+})
+export class ParkingSpotList {
+
+}

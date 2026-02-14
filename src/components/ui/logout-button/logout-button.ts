b7ff9d673b6cdcbf556966f0b961a0a2404af1c4
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-14 05:22:36 +0400

    Implement LogoutButton Component Functionality

--- src/components/ui/logout-button/logout-button.ts
+++ src/components/ui/logout-button/logout-button.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
-  selector: 'app-logout-button',
+  selector: 'button[logout]',
   imports: [],
   templateUrl: './logout-button.html',
   styleUrl: './logout-button.css',
+  host: {
+    '(click)': 'onClick()',
+  },
 })
 export class LogoutButton {
+  pressed = output<void>();
 
+  onClick() {
+    this.pressed.emit();
+  }
 }

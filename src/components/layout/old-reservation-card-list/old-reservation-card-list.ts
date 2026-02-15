6f39fce6a4e96d53f551ca6e46cc79c2375f5eaa
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 19:43:07 +0400

    Create OldReservationCardList Component

---
+++ src/components/layout/old-reservation-card-list/old-reservation-card-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-old-reservation-card-list',
+  imports: [],
+  templateUrl: './old-reservation-card-list.html',
+  styleUrl: './old-reservation-card-list.css',
+})
+export class OldReservationCardList {
+
+}

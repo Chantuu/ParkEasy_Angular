32d5a38afe31438f45bb6deb4e221f887ebf53b5
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 20:47:33 +0400

    Create ActiveReservationCard Component

---
+++ src/components/ui/active-reservation-card/active-reservation-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-reservation-card',
+  imports: [],
+  templateUrl: './active-reservation-card.html',
+  styleUrl: './active-reservation-card.css',
+})
+export class ActiveReservationCard {
+
+}

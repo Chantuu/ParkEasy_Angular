eed2e04b96751e7735e61c7fdf83f1759268cf84
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 16:59:23 +0400

    Implement ParkingSpotList Component Design

--- src/components/layout/parking-spot-list/parking-spot-list.ts
+++ src/components/layout/parking-spot-list/parking-spot-list.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ParkingSpotCard } from '../../ui/parking-spot-card/parking-spot-card';
+import { ParkingSpotStatusEnum } from '../../../utilities/enums/parking-spot-status.enum';
 
 @Component({
   selector: 'app-parking-spot-list',
-  imports: [],
+  imports: [ParkingSpotCard],
   templateUrl: './parking-spot-list.html',
   styleUrl: './parking-spot-list.css',
 })
 export class ParkingSpotList {
-
+  ParkingSpotStatus = ParkingSpotStatusEnum;
 }

9db89102368a9d416ebebb765bae7f723bc1742c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 00:35:41 +0400

    Implement ActiveReservationCard Component Design

--- src/components/ui/active-reservation-card/active-reservation-card.ts
+++ src/components/ui/active-reservation-card/active-reservation-card.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { returnFormattedDate } from '../../../utilities/functions/return-formatted-date.function';
+import { returnFormattedTime } from '../../../utilities/functions/return-formatted-time.function';
 
 @Component({
   selector: 'app-active-reservation-card',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './active-reservation-card.css',
 })
 export class ActiveReservationCard {
+  spotTitle = input.required<string>();
+  dateText = input.required<string>();
+  amount = input.required<number>();
 
+  returnFormattedDate = returnFormattedDate;
+  returnFormattedTime = returnFormattedTime;
 }

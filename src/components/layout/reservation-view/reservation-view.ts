923553cd24ec16410a4f861d3c72b0a54c3b4546
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 00:59:38 +0400

    Implement ReservationView Component Design

--- src/components/layout/reservation-view/reservation-view.ts
+++ src/components/layout/reservation-view/reservation-view.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { OldReservationCardList } from '../old-reservation-card-list/old-reservation-card-list';
+import { ActiveReservationCard } from '../../ui/active-reservation-card/active-reservation-card';
+import { ReservationInterface } from '../../../utilities/interfaces/reservation.interface';
 
 @Component({
   selector: 'app-reservation-view',
-  imports: [],
+  imports: [OldReservationCardList, ActiveReservationCard],
   templateUrl: './reservation-view.html',
   styleUrl: './reservation-view.css',
+  host: {
+    class: 'globalContainerXMargin',
+  },
 })
 export class ReservationView {
+  activeReservationData = signal<ReservationInterface | null>(null);
 
+  // TODO: Create Proper Reservation Retrieval
 }

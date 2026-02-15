17d67ea2d33a4bea08eeaadd63771f2868bb3a9b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 18:58:37 +0400

    Create ReservationView Component

---
+++ src/components/layout/reservation-view/reservation-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-view',
+  imports: [],
+  templateUrl: './reservation-view.html',
+  styleUrl: './reservation-view.css',
+})
+export class ReservationView {
+
+}

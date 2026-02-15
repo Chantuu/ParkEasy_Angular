f5d876c4f9aecdba6f68906c4fd24801b4b7bdc6
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 19:43:26 +0400

    Create OldReservationCard Component

---
+++ src/components/ui/old-reservation-card/old-reservation-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-old-reservation-card',
+  imports: [],
+  templateUrl: './old-reservation-card.html',
+  styleUrl: './old-reservation-card.css',
+})
+export class OldReservationCard {
+
+}

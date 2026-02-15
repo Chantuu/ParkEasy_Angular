cc5fa5052a3e550a0861024f1dca387d1b4b4488
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 00:56:17 +0400

    Implement OldReservationCardList

--- src/components/layout/old-reservation-card-list/old-reservation-card-list.ts
+++ src/components/layout/old-reservation-card-list/old-reservation-card-list.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { OldReservationCard } from '../../ui/old-reservation-card/old-reservation-card';
+import { ReservationEnum } from '../../../utilities/enums/reservation-status.enum';
+import { ReservationInterface } from '../../../utilities/interfaces/reservation.interface';
 
 @Component({
   selector: 'app-old-reservation-card-list',
-  imports: [],
+  imports: [OldReservationCard],
   templateUrl: './old-reservation-card-list.html',
   styleUrl: './old-reservation-card-list.css',
 })
 export class OldReservationCardList {
+  oldReservations = signal<ReservationInterface[]>([]);
 
+  // TODO: Implement proper Reservation Retrieval functionality
 }

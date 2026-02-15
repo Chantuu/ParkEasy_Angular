b97a4ebab7cb19e540f1d9f4cfc065fd67fd12b2
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 18:54:51 +0400

    Implement HomeScreenContainer Design

--- src/components/layout/home-screen-container/home-screen-container.ts
+++ src/components/layout/home-screen-container/home-screen-container.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TopBar } from '../../ui/top-bar/top-bar';
+import { NavBarLink } from '../../ui/nav-bar-link/nav-bar-link';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-screen-container',
-  imports: [],
+  imports: [TopBar, NavBarLink, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './home-screen-container.html',
   styleUrl: './home-screen-container.css',
 })
-export class HomeScreenContainer {
-
-}
+export class HomeScreenContainer {}

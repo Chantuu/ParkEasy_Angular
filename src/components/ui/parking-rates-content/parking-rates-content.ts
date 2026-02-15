bcc61c4796682546362fa9f817619e5efaab20c9
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 17:49:37 +0400

    Create ParkingRatesContent Component

---
+++ src/components/ui/parking-rates-content/parking-rates-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-rates-content',
+  imports: [],
+  templateUrl: './parking-rates-content.html',
+  styleUrl: './parking-rates-content.css',
+})
+export class ParkingRatesContent {
+
+}

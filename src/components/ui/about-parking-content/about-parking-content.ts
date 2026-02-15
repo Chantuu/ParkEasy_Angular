564699c6fbe0b55b4fee1844fe7b84a627d5073e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 17:48:53 +0400

    Create AboutParkingContent Component

---
+++ src/components/ui/about-parking-content/about-parking-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-parking-content',
+  imports: [],
+  templateUrl: './about-parking-content.html',
+  styleUrl: './about-parking-content.css',
+})
+export class AboutParkingContent {
+
+}

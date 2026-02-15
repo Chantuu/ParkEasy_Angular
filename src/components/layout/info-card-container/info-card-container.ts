df0bf2fc862ab798b3c82256f9457ce84cfc2ef1
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 18:53:41 +0400

    Implement InfoCardContainer Component Design

--- src/components/layout/info-card-container/info-card-container.ts
+++ src/components/layout/info-card-container/info-card-container.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-info-card-container',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './info-card-container.css',
 })
 export class InfoCardContainer {
-
+  title = input.required<string>();
 }

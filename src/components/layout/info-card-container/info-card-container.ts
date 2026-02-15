4ddd24e1b909e079504d9c677a431a88e02cce62
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 17:48:26 +0400

    Create InfoCardContainer Component

---
+++ src/components/layout/info-card-container/info-card-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-card-container',
+  imports: [],
+  templateUrl: './info-card-container.html',
+  styleUrl: './info-card-container.css',
+})
+export class InfoCardContainer {
+
+}

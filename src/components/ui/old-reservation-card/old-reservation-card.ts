1ce2a98efa28d167ce04b5e199bb35bcf831dd47
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 20:44:29 +0400

    Implement OldReservationCard Design

--- src/components/ui/old-reservation-card/old-reservation-card.ts
+++ src/components/ui/old-reservation-card/old-reservation-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ReservationEnum } from '../../../utilities/enums/reservation-status.enum';
 
 @Component({
   selector: 'app-old-reservation-card',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './old-reservation-card.css',
 })
 export class OldReservationCard {
+  spotTitle = input.required<string>();
+  dateText = input.required<string>();
+  status = input.required<ReservationEnum>();
+  amount = input.required<number>();
 
+  ReservationEnum = ReservationEnum;
+
+  returnFormattedDate() {
+    return new Date(this.dateText()).toLocaleString('en-US', {
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric',
+    });
+  }
 }

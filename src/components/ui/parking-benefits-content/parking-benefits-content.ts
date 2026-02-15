243c23260c03d3ef7b3d4cf4dc419e87d3cd6834
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-15 17:49:19 +0400

    Create ParkingBenefitsContent Component

---
+++ src/components/ui/parking-benefits-content/parking-benefits-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-benefits-content',
+  imports: [],
+  templateUrl: './parking-benefits-content.html',
+  styleUrl: './parking-benefits-content.css',
+})
+export class ParkingBenefitsContent {
+
+}

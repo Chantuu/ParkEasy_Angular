11b3841657e7dd3a598545429db49af669e1db0c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-17 00:51:54 +0400

    Rename ReservationInterface to InactiveReservationInterface
      This rename was done to properly reflect inactive (cancelled or completed) interface's data structure.

--- src/utilities/interfaces/inactive-reservation.interface.ts
+++ src/utilities/interfaces/inactive-reservation.interface.ts
@@ -1,9 +1,10 @@
 import { ReservationEnum } from '../enums/reservation-status.enum';
 
-export interface ReservationInterface {
+export interface InactiveReservationInterface {
   id: string;
   userId: string;
   parkingSpotName: string;
   startTime: string;
   status: ReservationEnum;
+  amount: number;
 }

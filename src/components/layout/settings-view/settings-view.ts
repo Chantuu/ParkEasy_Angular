5743e836e229d41042463b84c76368771f45485e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 20:26:44 +0400

    Implement Card Deletion Functionality in SettingsView Component

--- src/components/layout/settings-view/settings-view.ts
+++ src/components/layout/settings-view/settings-view.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { PaymentCardService } from './../../../services/payment-card-service';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { PaymentCardForm } from '../../ui/payment-card-form/payment-card-form';
 import { PaymentCard } from '../../ui/payment-card/payment-card';
+import { PaymentCardInterface } from '../../../utilities/interfaces/payment-card.interface';
 
 @Component({
   selector: 'app-settings-view',
@@ -11,6 +13,31 @@ import { PaymentCard } from '../../ui/payment-card/payment-card';
     class: 'globalContainerXMargin',
   },
 })
-export class SettingsView {
-  currentPaymentCard = signal<boolean>(true);
+export class SettingsView implements OnInit {
+  _paymentCardService = inject(PaymentCardService);
+
+  ngOnInit(): void {
+    this._paymentCardService.getCurrentPaymentCard().subscribe({
+      next: (response) => {
+        this.currentPaymentCard.set(response.data);
+      },
+      error: (error) => {
+        this.currentPaymentCard.set(null);
+        console.error(error);
+      },
+    });
+  }
+
+  currentPaymentCard = signal<PaymentCardInterface | null>(null);
+
+  onDeletePaymentCard() {
+    this._paymentCardService.deleteCurrentPaymentCard().subscribe({
+      next: () => {
+        this.currentPaymentCard.set(null);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }

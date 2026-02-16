df650d6a8a461993782e33c5a2ea0ec3a8765380
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 17:40:34 +0400

    Implement SettingsView Component Design

--- src/components/layout/settings-view/settings-view.ts
+++ src/components/layout/settings-view/settings-view.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { PaymentCardForm } from '../../ui/payment-card-form/payment-card-form';
+import { PaymentCard } from '../../ui/payment-card/payment-card';
 
 @Component({
   selector: 'app-settings-view',
-  imports: [],
+  imports: [PaymentCardForm, PaymentCard],
   templateUrl: './settings-view.html',
   styleUrl: './settings-view.css',
+  host: {
+    class: 'globalContainerXMargin',
+  },
 })
 export class SettingsView {
-
+  currentPaymentCard = signal<boolean>(true);
 }

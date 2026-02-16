964154cde3aba451796a4af37eb206f5358894e7
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 15:33:24 +0400

    Create PaymentCardForm Component

---
+++ src/components/ui/payment-card-form/payment-card-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-card-form',
+  imports: [],
+  templateUrl: './payment-card-form.html',
+  styleUrl: './payment-card-form.css',
+})
+export class PaymentCardForm {
+
+}

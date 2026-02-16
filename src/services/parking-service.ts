ea0b7a7574be027d02111eef0433995ca9092217
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 22:05:34 +0400

    Create ParkingService
      Created this service to retrieve parking spot data.

---
+++ src/services/parking-service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { ParkingSpotInterface } from '../utilities/interfaces/parking-spot.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ParkingService {
+  constructor(private _apiService: ApiService) {}
+
+  streamParkingData() {
+    return this._apiService.streamData<ParkingSpotInterface[]>('parking/parkingSpots');
+  }
+}

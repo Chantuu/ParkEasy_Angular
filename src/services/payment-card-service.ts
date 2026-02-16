9ea1ed2b0e68f2aaf12e18a8b31411b8fc8487d4
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 20:24:25 +0400

    Implement PaymentCardService Functionality
      Implemented methods for getting and deleting current payment card.

--- src/services/payment-card-service.ts
+++ src/services/payment-card-service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { PaymentCardResponseInterface } from '../utilities/interfaces/responses/payment-card-response.interface';
+import { DeleteCurrentPaymentCardResponseInterface } from '../utilities/interfaces/responses/delete-current-payment-card-response.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentCardService {
-  
+  constructor(private _apiService: ApiService) {}
+
+  getCurrentPaymentCard() {
+    return this._apiService.sendGetRequest<PaymentCardResponseInterface>('payments/paymentCard');
+  }
+
+  deleteCurrentPaymentCard() {
+    return this._apiService.sendDeleteRequest<DeleteCurrentPaymentCardResponseInterface>(
+      'payments/paymentCard',
+    );
+  }
 }

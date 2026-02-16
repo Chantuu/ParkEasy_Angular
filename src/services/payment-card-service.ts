1ee522a1afce54f1d20fd62e7c7e057678a5a69d
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 20:57:12 +0400

    Implement PaymentCard Addition in PaymentCardService

--- src/services/payment-card-service.ts
+++ src/services/payment-card-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api-service';
 import { PaymentCardResponseInterface } from '../utilities/interfaces/responses/payment-card-response.interface';
 import { DeleteCurrentPaymentCardResponseInterface } from '../utilities/interfaces/responses/delete-current-payment-card-response.interface';
+import { PaymentCardFormInterface } from '../utilities/interfaces/forms/payment-card-form.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,13 @@ export class PaymentCardService {
 
   getCurrentPaymentCard() {
     return this._apiService.sendGetRequest<PaymentCardResponseInterface>('payments/paymentCard');
+  }
+
+  createPaymentCard(paymentCardData: PaymentCardFormInterface) {
+    return this._apiService.sendPostRequest<PaymentCardFormInterface, PaymentCardResponseInterface>(
+      'payments/paymentCard',
+      paymentCardData,
+    );
   }
 
   deleteCurrentPaymentCard() {

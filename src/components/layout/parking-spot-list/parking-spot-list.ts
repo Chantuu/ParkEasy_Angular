0d6e7bce732c6abc4e5d1156e8f15f015dafc234
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-16 22:09:50 +0400

    Implement Dynamic Parking Spot Data Retrieval in ParkingSpotList Component
      Implemented functionality where frontend dynamically retrieves parking spot data from the backend using sse endpoint.

--- src/components/layout/parking-spot-list/parking-spot-list.ts
+++ src/components/layout/parking-spot-list/parking-spot-list.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ParkingSpotCard } from '../../ui/parking-spot-card/parking-spot-card';
 import { ParkingSpotStatusEnum } from '../../../utilities/enums/parking-spot-status.enum';
+import { Subscription } from 'rxjs';
+import { ParkingSpotInterface } from '../../../utilities/interfaces/parking-spot.interface';
+import { ParkingService } from '../../../services/parking-service';
 
 @Component({
   selector: 'app-parking-spot-list',
@@ -9,5 +12,25 @@ import { ParkingSpotStatusEnum } from '../../../utilities/enums/parking-spot-sta
   styleUrl: './parking-spot-list.css',
 })
 export class ParkingSpotList {
+  _parkingService = inject(ParkingService);
+  private _subscription!: Subscription;
+
+  parkingSpots = signal<ParkingSpotInterface[]>([]);
+
   ParkingSpotStatus = ParkingSpotStatusEnum;
+
+  ngOnInit(): void {
+    this._subscription = this._parkingService.streamParkingData().subscribe({
+      next: (data) => {
+        this.parkingSpots.set(data);
+      },
+      error: (error) => {
+        this.parkingSpots.set([]);
+        console.error(error);
+      },
+    });
+  }
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
 }

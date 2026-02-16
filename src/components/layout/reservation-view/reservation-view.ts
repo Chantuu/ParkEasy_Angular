3dc3132aa9444d2beedbe25dd5d54756625246ec
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-17 02:28:56 +0400

    Implement Active Reservation Retrieval From Backend

--- src/components/layout/reservation-view/reservation-view.ts
+++ src/components/layout/reservation-view/reservation-view.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { OldReservationCardList } from '../old-reservation-card-list/old-reservation-card-list';
 import { ActiveReservationCard } from '../../ui/active-reservation-card/active-reservation-card';
 import { ReservationInterface } from '../../../utilities/interfaces/reservation.interface';
+import { ReservationService } from '../../../services/reservation-service';
 
 @Component({
   selector: 'app-reservation-view',
@@ -12,8 +13,27 @@ import { ReservationInterface } from '../../../utilities/interfaces/reservation.
     class: 'globalContainerXMargin',
   },
 })
-export class ReservationView {
+export class ReservationView implements OnInit {
+  private _reservationService = inject(ReservationService);
+
   activeReservationData = signal<ReservationInterface | null>(null);
 
-  // TODO: Create Proper Reservation Retrieval
+  ngOnInit() {
+    this._reservationService.getActiveReservation().subscribe({
+      next: (data) => {
+        const currentData = data;
+
+        if (currentData === 'null') {
+          const parsedData = JSON.parse(currentData) as null;
+          this.activeReservationData.set(parsedData);
+        } else {
+          this.activeReservationData.set(currentData as ReservationInterface);
+        }
+      },
+      error: (error) => {
+        this.activeReservationData.set(null);
+        console.error(error);
+      },
+    });
+  }
 }
--- src/services/reservation-service.ts
+++ src/services/reservation-service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api-service';
 import { InactiveReservationsResponseInterface } from '../utilities/interfaces/responses/inactive-reservations-response.interface';
+import { ReservationInterface } from '../utilities/interfaces/reservation.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationService {
   constructor(private _apiService: ApiService) {}
+
+  getActiveReservation() {
+    return this._apiService.streamData<ReservationInterface | 'null'>('reservation');
+  }
 
   getOldReservations() {
     return this._apiService.sendGetRequest<InactiveReservationsResponseInterface>(

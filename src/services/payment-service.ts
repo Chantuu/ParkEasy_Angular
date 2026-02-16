de94ec3f7f61e3aa8f48220131a8554076acc788
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-17 03:54:29 +0400

    Implement PaymentService functionality
      Fully implemented method for calculating reservation price and for paying reservation in both cases: when checking in and when cancelling it.

--- src/services/payment-service.ts
+++ src/services/payment-service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { CalculateReservationPriceResponseInterface } from '../utilities/interfaces/responses/calculate-reservation-price-response.interface';
+import { ReservationEnum } from '../utilities/enums/reservation-status.enum';
+import { PayReservationRequestBodyInterface } from '../utilities/interfaces/request-bodies/pay-reservation-request-body.interface';
+import { PaymentResponseInterface } from '../utilities/interfaces/responses/payment-response.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentService {
-  
+  constructor(private _apiService: ApiService) {}
+
+  calculateReservationPrice() {
+    return this._apiService.sendGetRequest<CalculateReservationPriceResponseInterface>(
+      'reservation/calculatePrice',
+    );
+  }
+
+  payCompleted(amountToPay: number) {
+    return this._apiService.sendPostRequest<
+      PayReservationRequestBodyInterface,
+      PaymentResponseInterface
+    >('payments/pay', {
+      reservationStatus: ReservationEnum.COMPLETED,
+      amount: amountToPay,
+    });
+  }
+
+  payCancelled() {
+    return this._apiService.sendPostRequest<
+      PayReservationRequestBodyInterface,
+      PaymentResponseInterface
+    >('payments/pay', {
+      reservationStatus: ReservationEnum.CANCELLED,
+    });
+  }
 }

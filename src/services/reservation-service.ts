1e5d60618998acb7cef726e17e7c59603755abf4
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-17 00:55:20 +0400

    Implement Old Reservation Retrieval Function in ReservationService

--- src/services/reservation-service.ts
+++ src/services/reservation-service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { InactiveReservationsResponseInterface } from '../utilities/interfaces/responses/inactive-reservations-response.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationService {
-  
+  constructor(private _apiService: ApiService) {}
+
+  getOldReservations() {
+    return this._apiService.sendGetRequest<InactiveReservationsResponseInterface>(
+      'reservation/inactive',
+    );
+  }
 }

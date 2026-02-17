4cc9006ed2de06e69c541adbd349f7cd75bbb368
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-17 04:15:57 +0400

    Implement Dynamic Logged Username Display in TopBar Component

--- src/components/ui/top-bar/top-bar.ts
+++ src/components/ui/top-bar/top-bar.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { LogoutButton } from '../logout-button/logout-button';
 import { AuthService } from '../../../services/auth-service';
 import { Router } from '@angular/router';
+import { UserInterface } from '../../../utilities/interfaces/user.interface';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,9 +13,22 @@ import { Router } from '@angular/router';
     class: 'globalContainerXMargin',
   },
 })
-export class TopBar {
+export class TopBar implements OnInit {
   private _authService = inject(AuthService);
   private _router = inject(Router);
+
+  currentUser = signal<UserInterface | null>(null);
+
+  ngOnInit() {
+    this._authService.checkAuth().subscribe({
+      next: (response) => {
+        this.currentUser.set(response.data);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 
   onLogoutPress() {
     this._authService.logOutUser().subscribe({
